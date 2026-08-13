49b4ec757d5913b3854bffe75e3eab42fcd8dad9
NISHANTH K <nishanthk.23csd@kongu.edu>
2026-08-13 14:03:45 +0530

    Time: 1 ms (97.53%) | Memory: 47.6 MB (8.67%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int totalsum=0;
+        for(int num:nums){
+            totalsum+=num;
+        }
+        int leftsum=0;
+        for(int i=0;i<nums.length;i++){
+            int right=totalsum-leftsum-nums[i];
+            if(leftsum==right){
+                return i;
+            }
+            leftsum+=nums[i];
+        }
+        return -1;
+    }
+}
